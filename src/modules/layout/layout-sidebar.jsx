04c175ecefa16c432cfa257533203ec448401b0e
inmaytide <inmaytide>
2023-07-21 13:14:33 +0800

    dev: main layout - 3

--- src/modules/layout/layout-sidebar.jsx
+++ src/modules/layout/layout-sidebar.jsx
@@ -10,11 +10,13 @@ export default function LayoutSidebar() {
                 label="First parent link"
                 icon={<IconGauge size="1rem" stroke={1.5} />}
                 childrenOffset={__CHILD_OFFSET__}
+                active={true}
+                variant="filled"
             >
                 <NavLink label="First child link" />
                 <NavLink label="Second child link" />
                 <NavLink label="Nested parent link" childrenOffset={__CHILD_OFFSET__}>
-                    <NavLink label="First child link" />
+                    <NavLink label="First child link"/>
                     <NavLink label="Second child link" />
                     <NavLink label="Third child link" />
                 </NavLink>
